0209deca86a404cd1bbaba8795303bbc3a255ae4
pikatenor <pikatenor@p1kachu.net>
2023-03-31 03:58:46 +0900

    add oracle cloud

---
+++ oracle/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaaxfprp3257tv44dwtyturey2g7l6uxiwdv5ha6g55wqxzsubvwmyq"
+}
---
+++ oracle/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = local.root_compartment_id
+  user_ocid        = var.user_ocid
+  private_key_path = var.api_rsa_key.private_key_path
+  fingerprint      = var.api_rsa_key.fingerprint
+  region           = "ap-tokyo-1"
+}
