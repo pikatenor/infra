8374a16be8ae547691fc3f5929190d53f139cae5
Ryotaro Ko <pikatenor@p1kachu.net>
2025-11-29 20:08:25 +0900

    add starlight32

--- contabo/main.tf
+++ contabo/main.tf
@@ -1,2 +1,3 @@
 resource "contabo_instance" "starlight30" {}
 resource "contabo_instance" "starlight31" {}
+resource "contabo_instance" "starlight32" {}
--- contabo/outputs.tf
+++ contabo/outputs.tf
@@ -13,3 +13,11 @@ output "starlight31_ip_addr" {
 output "starlight31_ipv6_addr" {
   value = [for i in contabo_instance.starlight31.ip_config : i.v6][0][0].ip
 }
+
+output "starlight32_ip_addr" {
+  value = [for i in contabo_instance.starlight32.ip_config : i.v4][0][0].ip
+}
+
+output "starlight32_ipv6_addr" {
+  value = [for i in contabo_instance.starlight32.ip_config : i.v6][0][0].ip
+}
